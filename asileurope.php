029a10d1bd332bdf7b9490b0fdf95af9befd998d
Franck Dupont <kyfr59@gmail.com>
2017-04-04 14:51:54 +0200

    Modification du titre affiché dans la liste des notices individuelles du backoffice

--- asileurope.php
+++ asileurope.php
@@ -92,6 +92,29 @@ function asileurope_custom_post_type()
                        ]
   );
 
+  // Création du type de contenu "asileurope_individuelle"    
+  register_post_type('asileurope_individu',
+                     [
+                         'labels'      => [
+                             'name'          => __('Notices individuelles'),
+                             'singular_name' => __('Notice individuelle'),
+                             'add_new_item'  => __('Ajouter une notice individuelle'),
+                         ],
+                        'public'             => true,
+                        'publicly_queryable' => true,
+                        'show_ui'            => true,
+                        'show_in_menu'       => true,
+                        'show_in_nav_menus'  => true,
+                        'query_var'          => true,
+                        'rewrite'            => array( 'slug' => 'notices-individuelles'),
+                        'capability_type'    => 'post',
+                        'has_archive'        => true,
+                        'hierarchical'       => false,
+                        'menu_position'      => null,
+                        'supports'           => array('title', 'genre_pluriel'),
+                       ]
+  );
+
   
   // Partage de la taxonomie "taxonomie_asileurope" entre les 3 types de contenus 
   register_taxonomy_for_object_type('taxonomie_asileurope', 'asileurope_lexico');
@@ -112,8 +135,31 @@ function asileurope_change_title_placeholder( $title ){
     $title = 'Titre complet de l\'oeuvre';
   } elseif ( 'asileurope_lexico' == $screen->post_type ) {
     $title = 'Terme lexical'; 
+  } elseif ( 'asileurope_individu' == $screen->post_type ) {
+    $title = 'Nom de l\'individu'; 
   }
   return $title;
 }
 
-add_filter( 'enter_title_here', 'asileurope_change_title_placeholder' );
+
+/* Ajoute le prénom au nom dans la liste des notices individuelle dans le backoffice */
+function asileurope_admin_title() {
+    add_filter(
+        'the_title',
+        'asileurope_construct_admin_title',
+        100,
+        2
+    );
+}
+
+function asileurope_construct_admin_title( $title, $id ) {
+    
+    $post = get_post($id);
+    if  ( 'asileurope_individu' == $post->post_type ) {
+      $prenom  = get_field('prenoms');
+      return $title .' '.$prenom;
+    }
+    return $title;
+}
+
+add_action('admin_head-edit.php', 'asileurope_admin_title');
